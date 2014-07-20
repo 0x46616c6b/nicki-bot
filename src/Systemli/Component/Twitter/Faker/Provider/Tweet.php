b62915f1e6f15996d99b3cdba2c4681f8b82d125
louis <louis@systemli.org>
2014-07-20 21:02:11 +0200

    new tweet messages

--- src/Systemli/Component/Twitter/Faker/Provider/Tweet.php
+++ src/Systemli/Component/Twitter/Faker/Provider/Tweet.php
@@ -14,6 +14,11 @@ class Tweet extends Base
         "@{{username}} wieso T-Shirt? Im Osten sagt man Nicki!",
         "@{{username}} das heißt Nicki!",
         "Was soll denn ein T-Shirt sein? Oder meint @{{username}} etwa ein Nicki?",
+        "@{{username}} es wäre utopisch, wenn du in Zukunft Nicki statt T-Shirt verwendest.",
+        "@{{username}} bitte streiche T-Shirt aus deinem Wortschatz und benutze wieder Nicki. Danke!",
+        "@{{username}} gegen den Trend, Nicki statt T-Shirt!",
+        "@{{username}} bitte verwende keine Begriffe des Klassenfeindes. Nicki statt T-Shirt!",
+        "@{{username}} früher war nicht alles schlecht. Nicki statt T-Shirt!",
     );
 
     public function tweet()
